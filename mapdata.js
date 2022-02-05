34e1c5a4d8878aa32b1aa7ee245615e1437c1679
mipoung <95519464+mipoung@users.noreply.github.com>
2022-02-05 23:42:14 +0900

    Add files via upload

--- mapdata.js
+++ mapdata.js
@@ -15,6 +15,18 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
+//
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+//
 
 // 키워드로 장소를 검색합니다
 searchPlaces();
@@ -1085,8 +1097,16 @@ function displayMarker(place) {
         path: [new kakao.maps.LatLng(37.41284219648703, 126.65709254835912), new kakao.maps.LatLng(37.409567412666064, 126.65879047532312), new kakao.maps.LatLng(37.40863211558093, 126.65628719163115)], 
 
         color: 'orange'
-    }
+    },
 
+    /* 
+    {
+        path: [new kakao.maps.LatLng(), new kakao.maps.LatLng()], 
+
+        color: 'orange'
+
+    }
+    */
     
     
 ]; 
