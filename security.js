c041151a58681defd041f6ebefcccb05d1ca199f
mipoung <95519464+mipoung@users.noreply.github.com>
2022-02-20 13:23:15 +0900

    Add files via upload

--- security.js
+++ security.js
@@ -1,7 +1,8 @@
-pwd = prompt ('비밀번호를 입력해 주세요');
+pwd = prompt ('인증 코드');
 if(pwd == 4035) {
-   alert('확인완료!')
+   
 } else {
     alert('인증된 사용자가 아닙니다')
-   window.location.href="history.html";
+   //window.location.href="history.html";
+   window.history.go(-1);
 }
