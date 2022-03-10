142d9756c26521dc9406f34139dfe47b477ce916
mipoung <95519464+mipoung@users.noreply.github.com>
2022-03-10 22:32:20 +0900

    Add files via upload

--- mapdata2.js
+++ mapdata2.js
@@ -1392,12 +1392,20 @@ var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerS
 
 function elasticityON2() {
 
-  
 for (var i = 0; i < positions.length; i ++) {
+
+
+  // 마커 이미지의 이미지 크기 입니다
+  var imageSize = new kakao.maps.Size(24, 35); 
+    
+  // 마커 이미지를 생성합니다    
+  var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+  
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng // 마커의 위치
+        , image : markerImage // 마커 이미지 
     });
 
     // 마커에 표시할 인포윈도우를 생성합니다 
